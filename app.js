dbd910038454f53ce73b27dbdbae0910c769b70d
Alan Tweedie <aetweedie@gmail.com>
2015-06-03 11:17:37 -0600

    Made a js module

---
+++ app.js
@@ -0,0 +1,5 @@
+var calculator = require('./lib.js')
+
+console.log(calculator.add(10,12));
+console.log(calculator.subtract(14,7));
+console.log(calculator.name)
---
+++ lib.js
@@ -0,0 +1,9 @@
+module.exports = {
+  add: function(a,b){
+    return a + b;
+  },
+  subtract: function(x,y){
+    return x - y;
+  },
+  name: 'bill'
+};
